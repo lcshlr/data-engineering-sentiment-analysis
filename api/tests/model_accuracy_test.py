a85fe43ed57a6bd8326044a98447b361bde58127
AlexisNorindrEFREI <alexis.norindr@efrei.net>
2021-12-13 14:23:24 +0100

    added locust test

--- api/tests/model_accuracy_test.py
+++ api/tests/model_accuracy_test.py
@@ -2,14 +2,8 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-df = pd.read_csv('resources/weather-agg-DFE.csv')
 
-# columns with all nan values
-df = df.drop("_canary", axis=1)
-df = df.drop("gold_answer", axis=1)
-
-
-def get_vader_score(sentence):
+def get_vader_sentence_compound(sentence):
     sid_obj = SentimentIntensityAnalyzer()
     compound = sid_obj.polarity_scores(sentence)['compound']
     if compound > 0.05:
@@ -20,10 +14,20 @@ def get_vader_score(sentence):
         return 0
 
 
-df['vader'] = df.apply(lambda x: get_vader_score(x['tweet_text']), axis=1)
+def test_model_accuracy():
+    df = pd.read_csv('api/tests/resources/weather-agg-DFE.csv')
 
-df['label'] = df['what_emotion_does_the_author_express_specifically_about_the_weather'] \
-    .apply(lambda x: 1 if x == 'Positive' else (None if x == 'Neutral / author is just sharing information' or x == 'Tweet not related to weather condition' else 0))
+    # columns with all nan values
+    df = df.drop("_canary", axis=1)
+    df = df.drop("gold_answer", axis=1)
 
-# Evaluate results
-print(f'Accuracy: {accuracy_score(df.dropna()["label"].values, df.dropna()["vader"].values)}')
+    df['vader'] = df.apply(lambda x: get_vader_sentence_compound(x['tweet_text']), axis=1)
+
+    df['label'] = df['what_emotion_does_the_author_express_specifically_about_the_weather'] \
+        .apply(lambda x: 1 if x == 'Positive' else (None if x == 'Neutral / author is just sharing information' or x == 'Tweet not related to weather condition' else 0))
+
+    # Evaluate results
+    accuracy = accuracy_score(df.dropna()["label"].values, df.dropna()["vader"].values)
+    print(f'Accuracy: {accuracy_score(df.dropna()["label"].values, df.dropna()["vader"].values)}')
+
+    assert accuracy > 0.75
